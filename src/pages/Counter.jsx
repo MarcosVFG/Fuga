7c04996f77f3a2e292a1f6bef8c0224f1f6a7f54
MarcosVFG <marcosmarkin55@gmail.com>
2023-10-30 02:32:40 -0300

    Finish

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -3,7 +3,7 @@ import styles from './Counter.module.css'
 export default function Counter ({ number, title }) {
     return (
         <div className={styles.main}>
-            <div> 
+            <div className={styles.container}> 
                 <p>{number}</p>
                 <h3>{title}</h3>
             </div>
